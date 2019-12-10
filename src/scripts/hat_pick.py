a2c02d3b12415d83eb23ef9e1ae2a58c69aeea89
phillem123 <phillip_lemaster@fanniemae.com>
2019-12-09 22:35:40 -0500

    Finished email functionality. I need to fix the random pairing algorithm, though.

--- src/scripts/hat_pick.py
+++ src/scripts/hat_pick.py
@@ -1,4 +1,10 @@
 import random
+import smtplib
+from email.headerregistry import Address
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import make_msgid
 from pathlib import Path
 
 path = Path(__file__).parent / "../../data/emails.txt"
@@ -26,6 +32,7 @@ class Hat_Pick:
         return self.names_to_picks
 
     def make_random_pairs(self, names_to_emails_list, name):
+        #TODO: Fix random pairing algorithm to prevent stalling 
         random_name = random.choice(names_to_emails_list)
 
         while random_name in self.names_to_picks.values() or random_name == name:
@@ -33,11 +40,72 @@ class Hat_Pick:
 
         self.names_to_picks[name] = random_name
                     
+    def iterate_through_names_and_call_email_picks(self):
+        for name in list(self.names_to_picks):
+            self.email_pick(name, self.names_to_emails[name])
+    
+    def email_pick(self, name, email):
+        mail_user = 'email@email.com'
+        mail_password = 'pass'
+        msg = self.create_email(name, email)
 
-    def send_picks(self):
-        pass
+        try:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(mail_user, mail_password)
+            server.send_message(msg)
+            server.close()
+            print('Email sent!')
+        except Exception as ex:
+            print('Something went wrong...')
+            print(ex)
 
+    def create_email(self, email, name):
+        email_id = email.split('@')[0]
+        domain = email.split('@')[1]
+        msg = EmailMessage()
+        msg['Subject'] = "Secret Santa 2019"
+        msg['From'] = Address("Buddy the Bot", "email", "email.com")
+        msg['To'] = Address(name, email_id, domain)
+        image = make_msgid()
 
-
-# hat_pick = Hat_Pick(path)
-# hat_pick.pick_from_hat()
+        msg.add_alternative("""\
+        <html>
+        <head></head>
+        <body>
+            <p>""" + 'Hi {}!'.format(name) + """</p>
+            <p>You are receiving this message because you are participating in the Family's 
+            2019 Secret Santa! Please let me formally introduce myself. My name is Buddy (no relation to Buddy the Elf), 
+            the Secret Santa bot. Your friend, Phillip, created me. What a great guy!
+            </p>
+            <p>
+            My job is to tell you to get 
+            </p>
+        <b>"""+ '{}'.format(self.names_to_picks[name])+"""</b> 
+            <p>
+            a $20 gift for Secret Santa this year.
+            </p>
+            <p>
+            I know I know. You must be thinking "But it's not a secret if Phillip created you! 
+            He must know everyone's respective secret santa," but that is where you are wrong!
+            Phillip thought about this when he created me, and he created an algorithm to randomly 
+            draw names for people without him knowing who drew whom.
+            </p>
+            <p>
+            Anyway, please remember to get a $20 gift for <b>"""+ '{}'.format(self.names_to_picks[name]) +"""</b>! 
+            I need to get going as I am very busy around this time of year.
+            </p>
+            <p>
+            Have a blessed Christmas!
+            </p>
+            <p>
+            - Buddy the Bot
+            </p>
+            <p>
+            01001101 01100101 01110010 01110010 01111001 00100000 01000011 01101000 
+            01110010 01101001 01110011 01110100 01101101 01100001 01110011
+            </p>
+        </body>
+        </html>
+        """.format(image=image[1:-1]), subtype='html')
+        return msg
--- src/tests/hat_pick_test.py
+++ src/tests/hat_pick_test.py
@@ -1,7 +1,8 @@
 import unittest
-from ..scripts.hat_pick import Hat_Pick
-
 from pathlib import Path
+from unittest import mock
+
+from ..scripts.hat_pick import Hat_Pick
 
 path = Path(__file__).parent / "./test_data/test_emails.txt"
 
@@ -31,7 +32,13 @@ class Hat_Pick_Test(unittest.TestCase):
             self.assertIn(names[name], dict_of_picks.values())
 
     def test_that_the_email_method_has_been_called_x_times(self):
-        pass
+        hat_pick = Hat_Pick(path)
+        names = self.populate_names_list(path)
+        hat_pick.pick_from_hat()
+        hat_pick.email_pick = mock.MagicMock()
+        #hat_pick.email_pick.server = mock.MagicMock()
+        hat_pick.iterate_through_names_and_call_email_picks()
+        self.assertEqual(hat_pick.email_pick.call_count, 4)
 
 if __name__ == "__main__":
     unittest.main()
